0123a4db8bbe63396e788e598f0d3ff43dba1d8c
shobha2080 <shobhasha899@gmail.com>
2025-12-29 13:09:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-let keys = document.querySelectorAll(".key");
-let whiteKey = document.querySelectorAll(".key.white");
-let blackKey = document.querySelectorAll(".key.black");
 
-
-
-
-keys.forEach(key => {
-    key.addEventListener("click", () => {
-        playnote(key);
-    }); 
-})
-
-
-
-document.addEventListener("keydown", e => {
-    if(e.repeat) return;
-    const key = e.key;
-    const whiteKeyPiano = WHITE.pianoOf(key);
-    const blackKeyPiano = BLACK.pianoOf(key);
-    if (whiteKeyPiano > -1) playnote(whiteKeys[whiteKeyPiano]);
-    if (blackKeyPiano > -1) playnote(blackKey[blackKeyPiano]);
-})
-
-function playnote(key){
-    let noteAudio = document.getElementById(key.dataset.note);
-    noteAudio.currentTime = 0;
-    noteAudio.volume = 1.0;
-    noteAudio.play();
-    key.classList.add('active');
-    noteAudio.addEventListener("ended", () => {
-     key.classList.remove("active");
+  var typed = new Typed(".auto-type",{
+      strings : ["Figma", "Photoshop", "Illustrator", "Adobe XD", "Skech"],
+      typeSpeed : 150,
+      backSpeed : 150,
+      loop : true
     })
-
-}
-
-
-
-
-document.title="ready"
- 
-
- 
